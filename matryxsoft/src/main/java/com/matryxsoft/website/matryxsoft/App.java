f6fb3ffdce8224c6ce8ce1d124a2720c1fa23f54
USER <USER@USER-PC>
2019-05-03 11:03:16 +0530

    First Commit

---
+++ matryxsoft/src/main/java/com/matryxsoft/website/matryxsoft/App.java
@@ -0,0 +1,13 @@
+package com.matryxsoft.website.matryxsoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ matryxsoft/src/test/java/com/matryxsoft/website/matryxsoft/helloworld.java
@@ -0,0 +1,23 @@
+package com.matryxsoft.website.matryxsoft;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class helloworld {
+	
+  @Test
+  public void MatryxsoftWebsite() 
+  {
+	  System.out.println ("Hello Matryxsoft");
+	  Assert.assertTrue(true);
+  }
+  
+  @Test
+  public void MatryxsoftWebsite2() 
+  {
+	  System.out.println ("Hello Matryxsoft");
+	  Assert.assertTrue(false);
+  }
+  
+}
